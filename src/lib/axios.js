894832170daa9dab051cf795541a8c6f1271e9f9
Dewaa <arisurya228@gmail.com>
2025-11-10 11:36:01 +0800

    deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 const apiURL = import.meta.env.VITE_API_URL;
+const serverURL = import.meta.env.VITE_SERVER_URL;
 
 export const axiosInstance = axios.create({
   baseURL: apiURL,
 });
 
 export const axiosServer = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: serverURL,
 });
